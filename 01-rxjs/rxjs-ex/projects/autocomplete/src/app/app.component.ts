e1ea108968685fb4afd29bc093ebc0b6467a527e
aviran <mor.aviran@gmail.com>
2020-06-15 22:48:23 +0300

    solve ex1 lesson 1

--- 01-rxjs/rxjs-ex/projects/autocomplete/src/app/app.component.ts
+++ 01-rxjs/rxjs-ex/projects/autocomplete/src/app/app.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   template: `
   <div>
     <form>
-      <input name="search" type="search" placeholder="Search..." />
+      <input
+        name="search"
+        type="search"
+        placeholder="Search..."
+        (input)="onSearchChange($event.target.value)"
+      />
     </form>
     <ul>
-      <li *ngFor="let item of items">
-        {{ item }}
+      <li *ngFor="let item of items | async">
+        {{ item | json}}
       </li>
     </ul>
   </div>
   `,
 })
 export class AppComponent {
-  public items: any[] = [];
+  items;
+  public onSearchChange(value){
+    this.items = this._todoS.search(value);
+  }
+
+  constructor(private _todoS : TodoService){ }
+
 }
 
